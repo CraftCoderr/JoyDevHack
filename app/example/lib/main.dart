6532ef7ff9b8c7ff58301ab32ca34eb59c5ed1b7
ConstML <k-malinov@bk.ru>
2021-05-22 16:13:00 +0200

    bars

--- app/example/lib/main.dart
+++ app/example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:example/navigation_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'rooms.dart';
 
 void main() {
@@ -10,14 +11,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static final ThemeData _themeData = ThemeData(
+    appBarTheme: const AppBarTheme(
+      brightness: Brightness.light,
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      centerTitle: true,
+    ),
+    primarySwatch: Colors.blue,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.black,
       ),
+    );
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    return MaterialApp(
+      theme: _themeData,
       // home: const RoomsPage(),
       home: const NavigationPage(),
     );
